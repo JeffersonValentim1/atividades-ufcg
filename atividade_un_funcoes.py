c49c2101b999c371a747a1717b3b92d4b460ab26
Jefferson Valentim <88254264+JeffersonValentim1@users.noreply.github.com>
2022-06-02 10:16:48 -0300

    01

---
+++ atividade_un_funcoes.py
@@ -0,0 +1,17 @@
+"""
+função que faz xxxxx
+"""
+
+
+def mensagem(remetente, destinatario):
+    msg  = f"---" + "\n"
+    msg += f"Olá {destinatario},\n" + "\n"
+    msg += f"Este é um exemplo simples de mala direta.\n" + "\n"
+    msg += f"Abraços,\n\n{remetente}."
+    return msg
+def main():
+    remetente = input("Remetente? ")
+    destinatario = input("Destinatário? ")
+    msg = mensagem(remetente, destinatario)
+    print(msg)
+main()
